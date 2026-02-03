b05bdce6e79e0ba42eae50315951bc9b259273fa
Mukesh Kumar <mk8701952@gmail.com>
2026-02-03 15:24:50 +0530

    vbn

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -1,6 +1,6 @@
 export const DOMAIN_LAYOUT_MAP = {
 //   "faridabad.example.com": "faridabad",
-  "propertydeler-gold-frontend.vercel.app": "gurgaon",
+  "www.propertydeler-gold-frontend.vercel.app": "gurgaon",
   // "localhost:3000": "hisar",
   // "localhost:3000": "faridabad"
 };
