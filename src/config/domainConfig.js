7794c05502e20a1ab7149108264e3297b7306ea8
Mukesh Kumar <mk8701952@gmail.com>
2026-02-03 15:20:30 +0530

    gurgaon

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -1,7 +1,7 @@
 export const DOMAIN_LAYOUT_MAP = {
 //   "faridabad.example.com": "faridabad",
-  // "localhost:3000": "gurgaon",
+  "localhost:3000": "gurgaon",
   // "localhost:3000": "hisar",
-  "localhost:3000": "faridabad"
+  // "localhost:3000": "faridabad"
 };
-export const DEFAULT_LAYOUT = "faridabad";
+export const DEFAULT_LAYOUT = "gurgaon";
