b243776cb2667e2b5fca492ff21849db124153b9
Mukesh Kumar <mk8701952@gmail.com>
2026-02-03 15:40:12 +0530

    asdfghj

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -1,7 +1,7 @@
 export const DOMAIN_LAYOUT_MAP = {
-  "www.propertydeler-gold-frontend-9wvp.vercel.app": "faridabad",
-  "www.propertydeler-gold-frontend.vercel.app": "gurgaon",
-  "www.propertydeler-gold-frontend-lp3d.vercel.app": "hisar",
+  "propertydeler-gold-frontend-9wvp.vercel.app": "faridabad",
+  "propertydeler-gold-frontend.vercel.app": "gurgaon",
+  "propertydeler-gold-frontend-lp3d.vercel.app": "hisar",
   "localhost:3000": "gurgaon"
 };
 export const DEFAULT_LAYOUT = "faridabad";
