0fd912202001f1eb6432a363309b70e1dfee635a
Mukesh Kumar <mk8701952@gmail.com>
2026-02-04 20:50:43 +0530

    errt

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -1,8 +1,8 @@
 export const DOMAIN_LAYOUT_MAP = {
   "propertydeler-gold-frontend-9wvp.vercel.app": "faridabad",
-  // "propertydeler-gold-frontend.vercel.app": "gurgaon",
+  "propertydeler-gold-frontend.vercel.app": "gurgaon",
   "propertydeler-gold-frontend-lp3d.vercel.app": "hisar",
-  "propertydeler-gold-frontend-xkw9.vercel.app": "gurgaon",
+  // "propertydeler-gold-frontend-xkw9.vercel.app": "gurgaon",
   "propertydeler-gold-frontend-33ts.vercel.app": "noida",
   "propertydeler-gold-frontend-k2da.vercel.app": "ambala",
   "propertydeler-gold-frontend-w423.vercel.app": "sonipate",
