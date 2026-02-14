148e641acf3ece49d483f7b9881aa3e1917f45c6
Mukesh Kumar <mk8701952@gmail.com>
2026-02-14 16:23:09 +0530

    fghj

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -5,11 +5,12 @@ export const DOMAIN_LAYOUT_MAP = {
   "propertydeler-gold-frontend-xkw9.vercel.app": "noida",
   "propertydeler-gold-frontend-k2da.vercel.app": "ambala",
   "propertydeler-gold-frontend-w423.vercel.app": "sonipate",
-
+  "https://propertydealerinhisar.com":"hisar",
+  "https://www.propertydealerinhisar.com":"hisar",
   "propertydeler-gold-frontend-yg8l.vercel.app": "delhi",
 
   "property-dealer-in-hisar-2.vercel.app": "hisar",
-
+"localhost:3000": "hisar",
 };
 
 export const DEFAULT_LAYOUT = "sonipate";
