41b83439dccae173841ffb0b0b4100ac039189ad
Mukesh Kumar <mk8701952@gmail.com>
2026-02-16 16:14:14 +0530

    uesdhd

--- src/templates/design3/pages/home/page.jsx
+++ src/templates/design3/pages/home/page.jsx
@@ -8,7 +8,7 @@ export default function HomePage({domain}) {
     <>
       {/* <Navbar /> */}
       <Hero />
-      {/* <DealersSection domain={domain} /> */}
+      <DealersSection domain={domain} />
       {/* <Footer /> */}
     </>
   );
