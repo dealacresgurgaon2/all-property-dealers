7bb330ec43f45ecde989c635e95d909b5c8abda7
Mukesh Kumar <mk8701952@gmail.com>
2026-02-27 14:58:50 +0530

    ertyuihjok

--- src/config/domainConfig.js
+++ src/config/domainConfig.js
@@ -25,7 +25,7 @@ export const DOMAIN_LAYOUT_MAP = {
     "https://www.projectwallah.live/":"hisar",
     "https://projectwallah.live/":"hisar",
   "property-dealer-in-hisar-2.vercel.app": "hisar",
-"localhost:3000": "gurgaon",
+"localhost:3000": "delhi",
 };
 
 export const DEFAULT_LAYOUT = "sonipat"
--- src/lib/googleVerification.js
+++ src/lib/googleVerification.js
@@ -1,4 +1,4 @@
 export const GOOGLE_VERIFICATION = {
-    "www.propertydealerindelhi.com": "aGEJwP4HfUnVh-xZ1ST7KBWsFo3UR5JoR3d11f1gcQ",
+    "www.propertydealerindelhi.com": "AaGEJwP4HfUnVh-xZ1ST7KBWsFo3UR5JoR3d11f1gcQ",
   "localhost": "GSC_CODE_DEFAULT",
 }
