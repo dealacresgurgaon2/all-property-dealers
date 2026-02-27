b575a8b20874166b0036a6baab91608a19e92578
Mukesh Kumar <mk8701952@gmail.com>
2026-02-27 15:02:33 +0530

    cvbnm,

--- src/lib/googleVerification.js
+++ src/lib/googleVerification.js
@@ -1,4 +1,5 @@
 export const GOOGLE_VERIFICATION = {
+    "www.propertydealerinfaridabad.com": "fmVbAjBh2-6v3QV5k6wVDAIgOpeakdS_R4D_s7U56AM",
     "www.propertydealerindelhi.com": "AaGEJwP4HfUnVh-xZ1ST7KBWsFo3UR5JoR3d11f1gcQ",
   "localhost": "GSC_CODE_DEFAULT",
 }
