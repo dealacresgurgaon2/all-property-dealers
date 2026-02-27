1fbf0a77e974a97ed6199549b56176d09e407b79
Mukesh Kumar <mk8701952@gmail.com>
2026-02-27 13:48:28 +0530

    fdghh

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,7 +54,7 @@ export default async function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* ✅ Google Tag Manager */}
+        {/* ✅ Google Tag Manager here */}
         <GoogleTagManager gtmId={gtmId} />
 
         {/* ✅ Google Analytics */}
