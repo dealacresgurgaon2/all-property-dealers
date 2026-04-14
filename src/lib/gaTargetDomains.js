6494605ed320750a5232f3859f279f2816af84a4
Mukesh Kumar <mk8701952@gmail.com>
2026-04-14 15:46:13 +0530

    kjhgvc

--- src/lib/gaTargetDomains.js
+++ src/lib/gaTargetDomains.js
@@ -2,7 +2,7 @@ export const GA_TARGET_DOMAINS ={
     "www.propertydealerindelhi.com":"G-6T20SR2ZDW",
      "www.propertydealerinfaridabad.com": "G-BLVG9GT2EH",
     "www.propertydealerinhisar.com": "G-415KNEWBS1",
-    "www.propertydealerinnoida.com": "",
+    "www.propertydealerinnoida.com": "G-TMWSGYGWWE",
     "www.propertydealeringurgaon.com": "",
     "localhost":"G-9Z3KZ5V8K7",
 
